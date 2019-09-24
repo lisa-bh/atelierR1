b4cda57ccfe42e982329cf4b5edeee2b03ebdd3b
lisa-bh <55747698+lisa-bh@users.noreply.github.com>
2019-09-24 15:58:34 +0200

    ate

---
+++ atelier1.R
@@ -0,0 +1,23 @@
+2 + 3
+14 * 25
+8 - 12
+-3/10
+2.35/12.2
+1-sqrt(2)
+sort(c(2,5,1,3))
+x<-2
+x
+tailles <- c(167, 192, 173, 174, 172, 167, 171, 185, 163, 170)
+tailles + 20
+tailles <- c(167, 192, 173, 174, 172, 167, 171, 185, 163, 170)
+tailles
+length(tailles) #biostat
+summary(tailles)
+tailles.m <- tailles/100
+tailles.m
+tailles^2
+sin(tailles)
+poids <- c(86, 74, 83, 50, 78, 66, 66, 51, 50, 55)
+length(poids)
+imc <- poids/tailles*100^2
+
